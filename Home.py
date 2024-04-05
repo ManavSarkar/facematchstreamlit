73abaf84381748e3153cedeb19c5925e417fb903
Manav Sarkar <42129636+ManavSarkar@users.noreply.github.com>
2024-04-05 08:46:52 +0530

    init

---
+++ Home.py
@@ -0,0 +1,95 @@
+import streamlit as st
+from PIL import Image
+import requests
+import base64
+import io
+import re
+
+def format_name(name):
+    formatted_name = name
+    
+    formatted_name = formatted_name.replace("\\", " ")
+    formatted_name = formatted_name.replace("/", " ")
+    formatted_name = formatted_name.replace("105_classes_pins_dataset", " ")
+    formatted_name = formatted_name.replace("bollywood_celeb_faces", " ")
+    formatted_name = formatted_name.replace(".jpg", " ")
+    formatted_name = formatted_name.replace(".png", " ")
+    formatted_name = formatted_name.replace(".jpeg", " ")
+    
+    # remove underscores
+    formatted_name = formatted_name.replace("_", " ")
+    # remove numbers
+    formatted_name = re.sub(r'\d', '', formatted_name)
+    formatted_name = formatted_name.replace("\\", " ")
+    # remove extra spaces
+    formatted_name = re.sub(r'\s+', ' ', formatted_name)
+    
+    if "pins" in formatted_name:
+        formatted_name = formatted_name.replace("pins", " ")
+        # substring first half
+        formatted_name = formatted_name[:int(len(formatted_name) / 2) + 1]
+    
+    # trim
+    formatted_name = formatted_name.strip()
+    # capitalize
+    formatted_name = formatted_name.capitalize()
+    
+    return formatted_name
+
+# Function to convert image to base64
+def convert_to_base64(uploaded_file):
+    # Read the file as bytes
+    image_bytes = uploaded_file.read()
+
+    # Encode the bytes to base64
+    base64_bytes = base64.b64encode(image_bytes)
+
+    # Convert bytes to string
+    base64_string = base64_bytes.decode('utf-8')
+
+    return base64_string
+
+# Function to send image to API and get processed image
+def process_image(image_base64):
+    api_endpoint = "https://manavsarkar07-trial.hf.space/predict-actor/"  # Replace this with your API endpoint
+    payload = {"base64_data": image_base64}
+    response = requests.post(api_endpoint, json=payload)
+    response_data = response.json()
+    return response_data
+
+# Streamlit app
+def main():
+    st.title("Image Processing App")
+
+    
+    image = st.camera_input("Upload Image")
+    if image is not None:
+        # Display image
+        # st.image(image, caption="Uploaded Image", use_column_width=True)
+        base64_string = convert_to_base64(image)
+        base64_string = "data:image/jpeg;base64," + base64_string
+        # confirm button
+        if st.button("Process Image"):
+            # loading bar
+            with st.spinner("Processing Image..."):
+
+                response_data = process_image(base64_string)
+                
+                # Get processed image
+                processed_image_base64 = response_data["celeb_image"]
+                
+                celebrity_name = response_data["celeb"]
+
+                predicted_age = response_data["res"]["age"]
+                predicted_gender = response_data["res"]["gender"]
+                # Display processed image
+                image = Image.open(io.BytesIO(base64.b64decode(processed_image_base64)))
+                st.image(image, caption="Processed Image", use_column_width=True)
+                st.write(f"Predicted Celebrity: {format_name(celebrity_name)}")
+                st.write(f"Predicted Age: {predicted_age}")
+                st.write(f"Predicted Gender: {predicted_gender}")
+
+
+           
+if __name__ == "__main__":
+    main()
---
+++ pages/About.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def about_page():
+    st.title("About")
+    st.write("Welcome to the Celeb Face Matching project!")
+    st.write("This project allows you to match your face with a celebrity and predict your age and gender.")
+    st.write("To get started, simply upload an image of your face and let the magic happen!")
+    st.write("We use advanced machine learning algorithms to analyze your facial features and compare them with a database of celebrity faces.")
+    st.write("Our model also predicts your age and gender based on facial characteristics.")
+    st.write("Please note that the accuracy of the predictions may vary and are for entertainment purposes only.")
+    st.write("Enjoy exploring the world of celebrity face matching!")
+    # made by
+    st.write("Made by: Manav Sarkar")
+    
+    
+about_page()
