797c7943bca0eda698e9ac4023da2fc52c7ad140
ManavSarkar <manavsarkar07@gmail.com>
2025-07-30 09:18:36 +0530

    file upload function

--- Home.py
+++ Home.py
@@ -59,19 +59,35 @@ def process_image(image_base64):
 
 # Streamlit app
 def main():
-    st.title("Image Processing App")
+    st.title("Celebrity Prediction App")
+    st.subheader("Check if your face matches with your favorite celebrity!")
 
     
-    image = st.camera_input("Upload Image")
+    # image = st.camera_input("Click a selfie")
+
+    # upload_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])    
+    
+    tab1, tab2 = st.tabs(["Camera", "Image Upload"])
+
+    with tab1:
+        image = st.camera_input("Click a selfie")
+
+    with tab2:
+        upload_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+
+    if upload_file is not None:
+        image = upload_file
+    
+    # If image is uploaded via camera or file uploader
     if image is not None:
         # Display image
         # st.image(image, caption="Uploaded Image", use_column_width=True)
         base64_string = convert_to_base64(image)
         base64_string = "data:image/jpeg;base64," + base64_string
         # confirm button
-        if st.button("Process Image"):
+        if st.button("Check Now!"):
             # loading bar
-            with st.spinner("Processing Image..."):
+            with st.spinner("Searching for celebrity..."):
 
                 response_data = process_image(base64_string)
                 
@@ -84,8 +100,8 @@ def main():
                 predicted_gender = response_data["res"]["gender"]
                 # Display processed image
                 image = Image.open(io.BytesIO(base64.b64decode(processed_image_base64)))
-                st.image(image, caption="Processed Image", use_column_width=True)
-                st.write(f"Predicted Celebrity: {format_name(celebrity_name)}")
+                st.image(image, caption=format_name(celebrity_name), use_container_width=True)
+                st.write(f"You look like {format_name(celebrity_name)}")
                 st.write(f"Predicted Age: {predicted_age}")
                 st.write(f"Predicted Gender: {predicted_gender}")
 
